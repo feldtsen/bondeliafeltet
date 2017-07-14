d82a677ee5a5cc8afda5aad6061d15d47eff36e1
feldtsen <feldtsen@gmail.com>
2017-07-14 22:29:05 +0200

    side menu test

--- src/MainContent.js
+++ src/MainContent.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 import {Card, CardTitle, CardText} from 'material-ui/Card';
 import {Grid, Row, Col} from 'react-flexbox-grid';
+import FadeIn from 'react-fade-in';
 
 export default class MainContent extends Component {
     constructor() {
@@ -85,34 +86,42 @@ export default class MainContent extends Component {
                     <Row around="xs">
 
                         <Col xs={12} md={4}>
-                            <Card style={{margin: '.3em 0 .3em 0'}}>
-                                <CardTitle style={{padding: '3%'}} title='Side'
-                                           subtitle='sidesub'/>
-                                <CardText style={{padding: '0 3% 3% 3%'}}>
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aperiam asperiores
-                                    dolor dolorem esse eum inventore ipsa itaque, laboriosam laborum libero odio,
-                                    reprehenderit soluta sunt totam ut, voluptatum. Fugit, hic?
-                                </CardText>
-                            </Card>
+                            <FadeIn>
+                                <Card style={{margin: '.3em 0 .3em 0'}}>
+                                    <CardTitle style={{padding: '3%'}} title='Side'
+                                               subtitle='sidesub'/>
+                                    <CardText style={{padding: '0 3% 3% 3%'}}>
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aperiam
+                                        asperiores
+                                        dolor dolorem esse eum inventore ipsa itaque, laboriosam laborum libero odio,
+                                        reprehenderit soluta sunt totam ut, voluptatum. Fugit, hic?
+                                    </CardText>
+                                </Card>
+                            </FadeIn>
                         </Col>
 
-                        <Col xs={12} md={8} style={{overflowY: this.props.meta.width > 700 ? 'scroll':'', height: this.props.meta.width > 700?this.props.meta.height *0.89:''}}>
-                            <Row >
-                                {
-                                    this.state.allNews.map((news, i) => {
-                                        return (
-                                            <Col xs={12} key={`news${i}`} style={{margin: '.3em 0 .3em 0'}}>
-                                                <Card key={`news${i}`}>
-                                                    <CardTitle style={{padding: '3%'}} title={news.title}
-                                                               subtitle={news.subtitle}/>
-                                                    <CardText style={{padding: '0 3% 3% 3%'}}>
-                                                        {news.text}
-                                                    </CardText>
-                                                </Card>
-                                            </Col>
-                                        )
-                                    })
-                                }
+                        <Col xs={12} md={8} style={{
+                            overflowY: this.props.meta.width > 1024 ? 'scroll' : '',
+                            height: this.props.meta.width > 1024 ? this.props.meta.height * 0.89 : ''
+                        }}>
+                            <Row>
+                                <Col xs={12}>
+                                    <FadeIn>
+                                        {
+                                            this.state.allNews.map((news, i) => {
+                                                return (
+                                                    <Card key={`news${i}`} style={{margin: '.3em 0 .3em 0'}}>
+                                                        <CardTitle style={{padding: '3%'}} title={news.title}
+                                                                   subtitle={news.subtitle}/>
+                                                        <CardText style={{padding: '0 3% 3% 3%'}}>
+                                                            {news.text}
+                                                        </CardText>
+                                                    </Card>
+                                                )
+                                            })
+                                        }
+                                    </FadeIn>
+                                </Col>
                             </Row>
                         </Col>
 
